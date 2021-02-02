ae02956a0f04df30864fe86bdd350cc0049930c6
Lukas Rohner <lukas.rohner@valtech.com>
2021-02-02 08:56:47 +0100

    Add terraform cluster setup

---
+++ terraform/output.tf
@@ -0,0 +1,69 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "storage_container_name" {
+  value = azurerm_storage_container.sc.name
+}
+
+output "kubernetes_cluster_name" {
+  value = azurerm_kubernetes_cluster.k8s.name
+}
+
+output "client_key" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_key
+}
+
+output "client_certificate" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.client_certificate
+}
+
+output "cluster_ca_certificate" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.cluster_ca_certificate
+}
+
+output "cluster_username" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.username
+}
+
+output "cluster_password" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.password
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config_raw
+}
+
+output "host" {
+  value = azurerm_kubernetes_cluster.k8s.kube_config.0.host
+}
+
+output "acr_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "admin_password" {
+  value = azurerm_container_registry.acr.admin_password
+}
+
+output "configure" {
+  value = <<CONFIGURE
+
+Run the following commands to configure kubernetes client:
+
+$ terraform output kube_config > ~/.kube/aksconfig
+$ export KUBECONFIG=~/.kube/aksconfig
+
+Test configuration using kubectl
+
+$ kubectl get nodes
+CONFIGURE
+}
