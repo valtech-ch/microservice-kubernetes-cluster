16e17f37e60f81906aa38c2ff1d919ae797ca077
biseraivanovska <bisera.ivanovska@codeflair.ch>
2021-02-09 10:58:50 +0100

    configuration improvments and fixed tests

--- persistence/src/test/java/ch/valtech/kubernetes/microservice/cluster/persistence/controller/PersistenceControllerTest.java
+++ persistence/src/test/java/ch/valtech/kubernetes/microservice/cluster/persistence/controller/PersistenceControllerTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,7 +31,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@Disabled
 @AutoConfigureMockMvc
 @SpringBootTest
 public class PersistenceControllerTest {
