b151078e65031a61c81d77e005f39bf3551c43d8
Lukas Rohner <lukas.rohner@valtech.com>
2021-04-13 10:15:08 +0200

    Added cloud function

---
+++ functions/src/main/java/ch/valtech/kubernetes/microservice/cluster/function/Config.java
@@ -0,0 +1,48 @@
+package ch.valtech.kubernetes.microservice.cluster.function;
+
+import com.microsoft.azure.functions.ExecutionContext;
+import java.util.Optional;
+import java.util.function.Function;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+
+@SpringBootApplication
+public class Config {
+
+  public static void main(String[] args) throws Exception {
+    SpringApplication.run(Config.class, args);
+  }
+
+  @Bean
+  public Function<Message<String>, String> echo() {
+    return message -> message.getPayload();
+  }
+
+  @Bean
+  public Function<Message<Optional<String>>, String> reverse() {
+    return message -> {
+      if (message.getPayload().isEmpty()) {
+        return "nothing";
+      }
+      String value = message.getPayload().get();
+      ExecutionContext context = (ExecutionContext) message.getHeaders().get("executionContext");
+      try {
+        String reversed = StringUtils.reverse(value);
+        if (context != null) {
+          context.getLogger().info(String.format("Function: %s is reversing %s", context.getFunctionName(), value));
+        }
+        return reversed;
+      } catch (Exception e) {
+        e.printStackTrace();
+        if (context != null) {
+          context.getLogger().severe("Function could not reverse incoming request");
+        }
+        return ("Function error: - bad request");
+      }
+    };
+  }
+
+}
