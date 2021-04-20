381b04dbd8c11056ba6e1e358cf8452a00990a47
Lukas Rohner <lukas.rohner@valtech.com>
2021-04-20 12:09:48 +0200

    Added custom Jwt decoder for filestorage service

--- file-storage/src/main/java/ch/valtech/kubernetes/microservice/cluster/filestorage/config/SecurityConfiguration.java
+++ file-storage/src/main/java/ch/valtech/kubernetes/microservice/cluster/filestorage/config/SecurityConfiguration.java
@@ -1,21 +1,11 @@
 package ch.valtech.kubernetes.microservice.cluster.filestorage.config;
 
-import static org.springframework.security.oauth2.jwt.JwtDecoders.fromIssuerLocation;
-import static org.springframework.security.oauth2.jwt.JwtValidators.createDefaultWithIssuer;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.core.OAuth2TokenValidator;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 @Configuration
@@ -23,20 +13,11 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-  private final Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>> oauthConfig;
-
-  public SecurityConfiguration(
-      @Value("${spring.security.oauth2.enabled:true}") boolean enabledOauth2,
-      @Value("${application.token.issuer}") String tokenIssuer) {
-    if (enabledOauth2) {
-      this.oauthConfig = oauthConfigEnabled(tokenIssuer);
-    } else {
-      this.oauthConfig = AbstractHttpConfigurer::disable;
-    }
-  }
-
   @Override
   public void configure(HttpSecurity http) throws Exception {
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+
     http.csrf().disable()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
@@ -44,18 +25,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .authorizeRequests()
         .anyRequest().authenticated()
         .and()
-        .oauth2ResourceServer(oauthConfig);
-  }
-
-  private Customizer<OAuth2ResourceServerConfigurer<HttpSecurity>> oauthConfigEnabled(String tokenIssuer) {
-    OAuth2TokenValidator<Jwt> jwtValidator = createDefaultWithIssuer(tokenIssuer);
-    NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) fromIssuerLocation(tokenIssuer);
-    jwtDecoder.setJwtValidator(jwtValidator);
-    jwtDecoder.setClaimSetConverter(new UsernameSubClaimAdapter());
-
-    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
-    return oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter).decoder(jwtDecoder));
+        .oauth2ResourceServer().jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter));
   }
 
 }
