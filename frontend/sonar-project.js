c75ee48368bd31da6f578647cd95168d690d4ce3
Lukas Rohner <lukas.rohner@valtech.com>
2021-05-07 10:45:30 +0200

    Added frontend sonar scan

---
+++ frontend/sonar-project.js
@@ -0,0 +1,17 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+sonarqubeScanner(
+    {
+      serverUrl:  process.argv[2],
+      token : process.argv[3],
+      options : {
+        'sonar.projectKey': 'microservice-kubernetes-cluster-frontend',
+        'sonar.projectName': 'Microservice Kubernetes Cluster Frontend',
+        'sonar.sourceEncoding': 'UTF-8',
+        'sonar.sources':  'src',
+        'sonar.tests':  'src',
+        'sonar.inclusions'  :  '**', // Entry point of your code
+        'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+        'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
+        'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml'
+      }
+    }, () => {});
