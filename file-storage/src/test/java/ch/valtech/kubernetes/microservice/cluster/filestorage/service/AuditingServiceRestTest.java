0faa0a8fc7d86b429ce532071da8e71ceda15977
Lukas Rohner <lukas.rohner@valtech.com>
2021-05-28 14:29:43 +0200

    Added auditing reactive webclient

--- file-storage/src/test/java/ch/valtech/kubernetes/microservice/cluster/filestorage/service/AuditingServiceRestTest.java
+++ file-storage/src/test/java/ch/valtech/kubernetes/microservice/cluster/filestorage/service/AuditingServiceRestTest.java
@@ -12,26 +12,49 @@ import static org.springframework.test.web.client.response.MockRestResponseCreat
 import ch.valtech.kubernetes.microservice.cluster.filestorage.config.JwtRestTemplateConfiguration;
 import ch.valtech.kubernetes.microservice.cluster.persistence.api.dto.Action;
 import ch.valtech.kubernetes.microservice.cluster.persistence.api.dto.MessageDto;
+import ch.valtech.kubernetes.microservice.cluster.persistence.api.grpc.SearchRequest;
 import java.io.IOException;
+import java.util.List;
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.client.MockRestServiceServer;
 import org.springframework.web.client.RestTemplate;
+import reactor.core.publisher.Flux;
 
 @ExtendWith(SpringExtension.class)
 class AuditingServiceRestTest {
 
+  private static MockWebServer mockWebServer;
+
+  private static String url;
+
   private final RestTemplate restTemplate = new JwtRestTemplateConfiguration()
       .jwtRestTemplate(new RestTemplateBuilder());
-  private final String url = "http://localhost:8081/api/v1/messages";
 
   private final AuditingService auditingService = new AuditingServiceRest(url, restTemplate);
+
   private MockRestServiceServer mockRestServiceServer;
+
+  @BeforeAll
+  static void beforeAll() {
+    mockWebServer = new MockWebServer();
+    url = mockWebServer.url("/api/v1/messages").toString();
+  }
+
+  @AfterAll
+  static void afterAll() throws IOException {
+    mockWebServer.close();
+  }
 
   @BeforeEach
   void mockAuditingCall() {
@@ -64,6 +87,21 @@ class AuditingServiceRestTest {
         .andRespond(withException(new IOException()));
     MessageDto response = auditingService.audit("test.txt", Action.UPLOAD);
     assertNull(response);
+  }
+
+  @Test
+  void testSearchMessages() {
+    mockWebServer.enqueue(new MockResponse()
+        .setResponseCode(200)
+        .setHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+        .setBody("{\"message\": \"test.txt uploaded successfully\"}"));
+
+    Flux<MessageDto> search = auditingService
+        .search(SearchRequest.newBuilder().setFilename("test.txt").setLimit(2).build());
+
+    List<MessageDto> response = search.collectList().block();
+    assertEquals(1, response.size());
+    assertEquals("test.txt uploaded successfully", response.get(0).getMessage());
   }
 
 }
