958c384ac2ca18e8bb1daa9342573782a823de6e
Lukas Rohner <lukas.rohner@valtech.com>
2021-06-07 10:47:22 +0200

    Fixed paths

--- frontend/src/keycloak/keycloak-plugin.js
+++ frontend/src/keycloak/keycloak-plugin.js
@@ -2,10 +2,7 @@ import Vue, { PluginObject } from 'vue';
 import Keycloak, { KeycloakConfig } from 'keycloak-js';
 
 const initOptions: KeycloakConfig = {
-  // url: process.env.VUE_APP_KEYCLOAK_URL,
-  // realm: process.env.VUE_APP_KEYCLOAK_REALM,
-  // clientId: process.env.VUE_APP_KEYCLOAK_CLIENT_ID,
-  url: 'https://vtch-aks-demo.duckdns.org/auth',
+  url: 'auth',
   realm: 'cluster',
   clientId: 'login-app',
   onLoad: 'login-required'
