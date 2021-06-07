fd07bdac05f0292f2023922f68e7b5af933d18d9
Lukas Rohner <lukas.rohner@valtech.com>
2021-06-07 10:11:34 +0200

    Code cleanup and changed log level

--- persistence/src/main/java/ch/valtech/kubernetes/microservice/cluster/persistence/config/KafkaErrorHandler.java
+++ persistence/src/main/java/ch/valtech/kubernetes/microservice/cluster/persistence/config/KafkaErrorHandler.java
@@ -26,17 +26,17 @@ final class KafkaErrorHandler implements ContainerAwareErrorHandler {
       String topic = record.topic();
       long offset = record.offset();
       int partition = record.partition();
-      if (thrownException.getClass().equals(DeserializationException.class)) {
+      if (thrownException instanceof DeserializationException) {
         DeserializationException exception = (DeserializationException) thrownException;
         String malformedMessage = new String(exception.getData());
-        log.info("Skipping message with topic {} and offset {} - malformed message: {} , exception: {}", topic, offset,
+        log.error("Skipping message with topic {} and offset {} - malformed message: {} , exception: {}", topic, offset,
             malformedMessage, exception.getLocalizedMessage());
       } else {
-        log.info("Skipping message with topic {} - offset {} - partition {} - exception {}", topic, offset, partition,
+        log.error("Skipping message with topic {} - offset {} - partition {} - exception {}", topic, offset, partition,
             thrownException);
       }
     } else {
-      log.info("Consumer exception - cause: {}", thrownException.getMessage());
+      log.error("Consumer exception - cause: {}", thrownException.getMessage());
     }
   }
 
