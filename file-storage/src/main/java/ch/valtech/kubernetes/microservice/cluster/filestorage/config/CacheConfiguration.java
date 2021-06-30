54b73f44c71e0ee1af6bad97acc051cde2eba874
Lukas Rohner <lukas.rohner@valtech.com>
2021-06-30 15:17:46 +0200

    Added hazelcast sidecar

---
+++ file-storage/src/main/java/ch/valtech/kubernetes/microservice/cluster/filestorage/config/CacheConfiguration.java
@@ -0,0 +1,59 @@
+package ch.valtech.kubernetes.microservice.cluster.filestorage.config;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
+import javax.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.support.NoOpCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Slf4j
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+
+  @PreDestroy
+  public void destroy() {
+    log.info("Closing Cache Manager");
+    Hazelcast.shutdownAll();
+  }
+
+  @Bean
+  @Profile("!dev")
+  public ClientConfig clientConfig() {
+    ClientConfig clientConfig = new ClientConfig();
+    ClientNetworkConfig networkConfig = clientConfig.getNetworkConfig();
+    networkConfig.addAddress("127.0.0.1:5701")
+        .setRedoOperation(true)
+        .setConnectionTimeout(5000);
+    return clientConfig;
+  }
+
+  @Bean
+  @Profile("!dev")
+  public HazelcastInstance hazelcastInstance(ClientConfig clientConfig) {
+    return HazelcastClient.newHazelcastClient(clientConfig);
+  }
+
+  @Bean
+  @Profile("!dev")
+  public CacheManager cacheManager(HazelcastInstance hazelcastInstance) {
+    log.debug("Starting HazelcastCacheManager");
+    return new HazelcastCacheManager(hazelcastInstance);
+  }
+
+  @Bean
+  @Profile("dev")
+  public CacheManager getNoOpCacheManager() {
+    return new NoOpCacheManager();
+  }
+
+}
