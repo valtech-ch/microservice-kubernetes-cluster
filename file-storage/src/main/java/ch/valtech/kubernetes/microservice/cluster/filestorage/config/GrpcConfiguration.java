b5db91bb5f486ebad2542d3acea13e317524d3e2
Lukas Rohner <lukas.rohner@valtech.com>
2022-12-03 22:10:51 +0100

    Fixed grpc-spring-boot-starter for spring boot 3

--- file-storage/src/main/java/ch/valtech/kubernetes/microservice/cluster/filestorage/config/GrpcConfiguration.java
+++ file-storage/src/main/java/ch/valtech/kubernetes/microservice/cluster/filestorage/config/GrpcConfiguration.java
@@ -8,9 +8,20 @@ import io.grpc.Metadata;
 import java.util.Optional;
 import java.util.concurrent.Executor;
 import net.devh.boot.grpc.client.inject.StubTransformer;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@ImportAutoConfiguration({
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientMetricAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientHealthAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientSecurityAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientTraceAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcDiscoveryClientAutoConfiguration.class,
+    net.devh.boot.grpc.common.autoconfigure.GrpcCommonCodecAutoConfiguration.class,
+    net.devh.boot.grpc.common.autoconfigure.GrpcCommonTraceAutoConfiguration.class
+})
 @Configuration
 public class GrpcConfiguration {
 
