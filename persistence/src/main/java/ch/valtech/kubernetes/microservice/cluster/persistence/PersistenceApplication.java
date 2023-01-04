7691b582a4f418c104ba4449531ebcdea273a7cd
Lukas Rohner <lukas.rohner@valtech.com>
2023-01-04 14:50:41 +0100

    Test

--- persistence/src/main/java/ch/valtech/kubernetes/microservice/cluster/persistence/PersistenceApplication.java
+++ persistence/src/main/java/ch/valtech/kubernetes/microservice/cluster/persistence/PersistenceApplication.java
@@ -1,16 +1,31 @@
 package ch.valtech.kubernetes.microservice.cluster.persistence;
 
+import io.grpc.netty.shaded.io.netty.buffer.AbstractByteBufAllocator;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"ch.valtech.kubernetes.microservice.cluster.persistence",
     "ch.valtech.kubernetes.microservice.cluster.security.config"})
+@ImportRuntimeHints(PersistenceApplicationRuntimeHints.class)
 public class PersistenceApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(PersistenceApplication.class, args);
+  }
+
+}
+
+class PersistenceApplicationRuntimeHints implements RuntimeHintsRegistrar {
+
+  @Override
+  public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+    hints.reflection().registerType(AbstractByteBufAllocator.class, MemberCategory.INVOKE_DECLARED_METHODS);
   }
 
 }
