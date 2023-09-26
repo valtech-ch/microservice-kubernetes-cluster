2aaebb833a186a2aa6c08017fde56fae4cb6197e
Lukas Rohner <lukas.rohner@valtech.com>
2023-09-26 16:56:37 +0200

    Moved to new jira instance

--- scripts/release-toggles/findJiraTickets.py
+++ scripts/release-toggles/findJiraTickets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import requests, json, sys;
 
-url = 'https://tracking.valtech.swiss/rest/api/latest/search?jql=project=VJAP+and+status+in+(%22Internal%20review%22%2C%20%22Ready%20for%20internal%20review%22)'
+url = 'https://jira.valtech.com/rest/api/latest/search?jql=project=VJAP+and+status+in+(%22Internal%20review%22%2C%20%22Ready%20for%20internal%20review%22)'
 headers = {'Authorization': 'Bearer ' + sys.argv[1]}
 respone = requests.get(url, headers=headers)
 
